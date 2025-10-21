51e714009dc06849556fac8801bea24cd4dc7676
Miracle Alex <collinsalex50@gmail.com>
2025-10-21 15:54:20 +0100

    Delete wisdom directory

--- wisdom/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from pages import views
-
-urlpatterns = [
-    path('', views.quotes, name='index'),
-    path('quotes', views.quote_list, name='quotes'),
-    path('quote/<int:id>', views.quote_detail, name='quote_detail'),
-]
--- wisdom/readme.md
+++
@@ -1,4 +0,0 @@
-A django web application to store and display motivation quotes, for self development and motivation
-
-# To deploy
-`git push heroku master`
