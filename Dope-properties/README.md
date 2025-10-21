7a4b2f1ff855d521c39b334bfc757a58a13efe80
Miracle Alex <collinsalex50@gmail.com>
2025-10-21 15:53:32 +0100

    Delete Dope-properties directory

--- Dope-properties/README.md
+++
@@ -1,4 +0,0 @@
-# Dope-properties
-A web page that where you can get property listings
-
-
--- Dope-properties/mysite/properties/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.properties, name='properties'),
-    path('<int:property_id>/', views.property_, name='property'),
-    path('search/', views.search, name='search')
-]
